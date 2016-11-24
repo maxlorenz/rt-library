2a45fada1cd87ed68151fce8e175b55b00da4fe5
Max Lorenz <max-lorenz@gmx.net>
2016-11-24 19:09:59 +0100

    Created a new npm project

---
+++ index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+let osmread = require("osm-read");
+
+let count = {
+    bounds: 0,
+    nodes: 0,
+    ways: 0,
+    relations: 0
+}
+
+osmread.parse({
+    filePath: './test/monaco.osm.pbf',
+
+    bounds:(b) => count.bounds++,
+    node: (n) => count.nodes++,
+    way: (w) => count.ways++,
+    relation: (r) => count.relations++,
+
+    endDocument: () => console.log(count),
+    error: (msg) => console.log('error: ' + msg) 
+});
